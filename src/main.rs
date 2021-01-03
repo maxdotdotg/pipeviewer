cce7e9b1df366a0cd0d9f771966b419ba9937b03
Max G <maxdotdotg@gmail.com>
2021-01-03 17:03:35 -0500

    main: rustfmt, update bytes read in-place instead of on completion

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::io::{self, ErrorKind, Result, Read, Write};
+use std::io::{self, ErrorKind, Read, Result, Write};
 
 // make a const for the buffer size
 const CHUNK_SIZE: usize = 16 * 1024;
@@ -16,14 +16,12 @@ fn main() -> Result<()> {
     // debug macro! it takes any expression!
     // dbg!(silent);
     let mut total_bytes = 0;
-    
 
     // make a buffer?
     let mut buffer = [0; CHUNK_SIZE];
 
     // loop through the bytes read, not just the limit of `buffer`
     loop {
-
         // `.read` take a fixed-size buffer and returns the number of bytes read on success
         // since this is variable assignment, use a semicolon
         let num_read = match io::stdin().read(&mut buffer) {
@@ -34,23 +32,23 @@ fn main() -> Result<()> {
         //dbg!(total_bytes += num_read);
         total_bytes += num_read;
 
+        // print total bytes as part of each loop,
+        // and do so from the beginning of the line to
+        // overwrite the value
+        if !silent {
+            eprint!("\r{}", total_bytes);
+        }
+
         // if writing to stdout throws an error, catch and print it
         if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
-            // if the error is BrokenPipe, then catch and ignore it since it's 
+            // if the error is BrokenPipe, then catch and ignore it since it's
             // mostly expected
             if e.kind() == ErrorKind::BrokenPipe {
-                break
+                break;
             }
-            return Err(e)
+            return Err(e);
         }
     }
 
-    // printing total_bytes to stderr, again, IDK
-    // using stderr to print additional info kinda bothers me? not sure....
-
-    // if dbg!(!silent) {
-    if !silent {
-        eprintln!("{}", total_bytes);
-    }
     Ok(())
 }
