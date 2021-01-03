e34df291d284e0609d8bb3bced4720763eb9411d
Max G <maxdotdotg@gmail.com>
2021-01-03 10:11:13 -0500

    main: use a loop to read more data on stdin than CHUNK_SIZE

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,26 @@ use std::io::{self, Read, Write};
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
-    // make a buffer?
-    let mut buffer = [0; CHUNK_SIZE];
+    let mut total_bytes = 0;
 
-    // read take a fixed size buffer and returns number of bytes read on success
-    // handle errors with unwrap, IDK
-    let num_read = io::stdin().read(&mut buffer).unwrap();
+    // loop through the bytes read, not just the limit of `buffer`
+    loop{
+        // make a buffer?
+        let mut buffer = [0; CHUNK_SIZE];
 
-    // printing num_read to stderr, again, IDK
-    eprintln!("num_read: {}", num_read);
+        // `.read` take a fixed-size buffer and returns the number of bytes read on success
+        // since this is variable assignment, use a semicolon
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        total_bytes += num_read;
 
-    // print all the things, unmodified, to stdout
-    io::stdout().write_all(&buffer[..num_read]).unwrap();
+        // print all the things, unmodified, to stdout
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+
+    // printing total_bytes to stderr, again, IDK
+    eprintln!("{}", total_bytes);
 }
